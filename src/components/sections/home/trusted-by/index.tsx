e66b710387e42564a17d1645f19993ec28973604
Matias Perez <matiasperezpc@hotmail.com>
2022-07-07 14:37:45 -0300

    Update mobile

--- src/components/sections/home/trusted-by/index.tsx
+++ src/components/sections/home/trusted-by/index.tsx
@@ -1,34 +1,51 @@
+import Image from 'next/future/image'
 import { FC } from 'react'
 
 import { Section, SectionHeading } from '~/components/common/section'
 import { Container } from '~/components/layout/container'
+import circleCiSvg from '~/public/images/logos/circle-ci.svg'
+import codeSandboxSvg from '~/public/images/logos/code-sandbox.svg'
+import excalidrawSvg from '~/public/images/logos/excalidraw.svg'
+import gitbookSvg from '~/public/images/logos/git-book.svg'
+import glideSvg from '~/public/images/logos/glide.svg'
+import replItSvg from '~/public/images/logos/repl-it.svg'
+import solidJsSvg from '~/public/images/logos/solid-js.svg'
+import xStateSvg from '~/public/images/logos/xstate.svg'
 
 import s from './trusted-by.module.scss'
 
 const logos = [
   {
-    title: 'Circle Ci'
+    title: 'Circle Ci',
+    logo: circleCiSvg
   },
   {
-    title: 'Gitbook'
+    title: 'Gitbook',
+    logo: gitbookSvg
   },
   {
-    title: 'Code Sandbox'
+    title: 'Code Sandbox',
+    logo: codeSandboxSvg
   },
   {
-    title: 'Repl.it'
+    title: 'Repl.it',
+    logo: replItSvg
   },
   {
-    title: 'Excalidraw'
+    title: 'Excalidraw',
+    logo: excalidrawSvg
   },
   {
-    title: 'Solid JS'
+    title: 'Solid JS',
+    logo: solidJsSvg
   },
   {
-    title: 'XState'
+    title: 'XState',
+    logo: xStateSvg
   },
   {
-    title: 'Glide'
+    title: 'Glide',
+    logo: glideSvg
   }
 ]
 
@@ -38,8 +55,10 @@ export const TrustedBy: FC = () => {
       <Container size="sm">
         <SectionHeading centered title="Trusted by top teams" />
         <div className={s['logos-grid']}>
-          {logos.map(({ title }) => (
-            <div className={s['logo']} title={title} key={title} />
+          {logos.map(({ title, logo }) => (
+            <div className={s['logo']} title={title} key={title}>
+              <Image src={logo} />
+            </div>
           ))}
         </div>
       </Container>
