9ddb60364a615304dd10ebfd4c10b3d5d7c44627
Matias Perez <matiasperezpc@hotmail.com>
2022-07-14 10:53:15 -0300

    remove duplicate elements

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,6 +1,4 @@
 import { Meta } from '~/components/common/meta'
-import { Footer } from '~/components/layout/footer'
-import { Header } from '~/components/layout/header'
 import { PageLayout } from '~/components/layout/page'
 import { Hero } from '~/components/sections/pricing/hero'
 import { Plans } from '~/components/sections/pricing/plans'
@@ -10,10 +8,8 @@ const Pricing = () => {
     <PageLayout>
       <Meta />
 
-      <Header />
       <Hero />
       <Plans />
-      <Footer />
     </PageLayout>
   )
 }
