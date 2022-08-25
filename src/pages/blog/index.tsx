00acb637cc4bab84887bcff0975b4f618c4dfd52
Travis Arnold <travis@replay.io>
2022-08-25 16:27:42 -0700

    pull posts into index

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,27 +1,55 @@
+import * as fs from 'fs'
+import matter from 'gray-matter'
+import type { GetStaticProps } from 'next'
+
 import { Heading } from '~/components/common/heading'
 import { Container } from '~/components/layout/container'
 import { PageLayout } from '~/components/layout/page'
 
-const BlogPage = () => {
+type Post = { title: string; content: string; slug: string }
+
+type Posts = Post[]
+
+const BlogPage = ({ posts }: { posts: Posts }) => {
   return (
     <PageLayout>
       <Container>
         <Heading as="h1" size="lg">
           Blog
         </Heading>
-        <Heading size="sm">Performant Icon Systems in React</Heading>
-        <p>
-          A task every Design Systems engineer faces at some point is how the
-          team will manage icons across design and development. I've been
-          through countless rewrites of icons over the years and seen the toll
-          it takes on development time, not to mention the team's morale. Today,
-          we'll look at speeding up this process to make an easy and scalable
-          icons solution for teams of any size while not sacrificing the user
-          experience.
-        </p>
+
+        {posts.map((post) => (
+          <div key={post.slug}>
+            <Heading as="h2" size="sm">
+              {post.title}
+            </Heading>
+            <p>{post.content}</p>
+          </div>
+        ))}
       </Container>
     </PageLayout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      posts: fs
+        .readdirSync('src/pages/blog')
+        .filter((filename) => filename.endsWith('.mdx'))
+        .map((filename) => {
+          const { content, data } = matter(
+            fs.readFileSync(`src/pages/blog/${filename}`, 'utf-8')
+          )
+
+          return {
+            content,
+            title: data.title,
+            slug: filename.replace('.mdx', '')
+          }
+        }) as Posts
+    }
+  }
 }
 
 export default BlogPage
