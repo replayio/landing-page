316157ed811f5987b3e7edb95f61bfe465ab87af
Julian Benegas <julianbenegas99@gmail.com>
2022-08-29 13:33:41 -0300

    try preloading hero fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/inter/Inter-italic.var.woff2"
+            as="font"
+          />
+          <link
+            rel="preload"
+            href="/fonts/inter/Inter-roman.var.woff2"
+            as="font"
+          />
+          <link
+            rel="preload"
+            href="/fonts/roobert/Roobert-Regular.woff2"
+            as="font"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
