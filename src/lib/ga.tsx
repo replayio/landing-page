0e5b42b837666aed07558fb2819d544088ce869b
Matias Perez <matiasperezpc@hotmail.com>
2022-09-13 23:48:00 -0300

    fix

--- src/lib/ga.tsx
+++ src/lib/ga.tsx
@@ -48,7 +48,6 @@ export const event = ({
 export const GAScripts = () => {
   return (
     <>
-      <link rel="preconnect" href="https://www.googletagmanager.com" />
       <Script
         strategy="afterInteractive"
         async
