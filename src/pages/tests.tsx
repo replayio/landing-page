189be2f8fc8bdee24fbaa55e1568adfa8997839a
Jason Laster <jason.laster.11@gmail.com>
2022-10-03 13:06:19 -0700

    Add test suite sign up flow (#75)

---
+++ src/pages/tests.tsx
@@ -0,0 +1,15 @@
+import { Meta } from '~/components/common/meta'
+import { PageLayout } from '~/components/layout/page'
+import { Hero } from '~/components/sections/tests/hero'
+
+const Tests = () => {
+  return (
+    <PageLayout>
+      <Meta />
+
+      <Hero />
+    </PageLayout>
+  )
+}
+
+export default Tests
