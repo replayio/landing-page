3a17fea36924c2e3fed1f566c9f9bfcbe304b83f
Dan Miller <dan@replay.io>
2023-01-18 14:00:30 -0800

    Remove QA Manager role

--- src/components/sections/about/work/index.tsx
+++ src/components/sections/about/work/index.tsx
@@ -28,12 +28,6 @@ const positions = [
     title: 'Backend Engineering Manager',
     skills: 'Management',
     href: 'https://replayio.notion.site/Backend-Engineering-Manager-bfb84da8872d493dbe7a891e420be408'
-  },
-
-  {
-    title: 'QA Manager',
-    skills: 'Management',
-    href: 'https://replayio.notion.site/Quality-Assurance-Manager-8f5936e6a79a44beb6c4de9ecdbb3344'
   }
 ]
 
