41ad57df196789f38ffa9d82254cfd608b1386d5
Jon Bell <github@lot23.com>
2023-02-07 03:02:23 +0000

    Adding Dom and Miriam

--- src/components/sections/about/team/team.ts
+++ src/components/sections/about/team/team.ts
@@ -10,6 +10,8 @@ import oliver from '~/public/images/about/team/oliver.jpg'
 import ryan from '~/public/images/about/team/ryan.png'
 import sophie from '~/public/images/about/team/sophie.png'
 import vaughn from '~/public/images/about/team/vaughn.jpeg'
+import miriam from '~/public/images/about/team/miriam.png'
+import domi from '~/public/images/about/team/domi.png'
 
 export const team = [
   {
@@ -29,15 +31,16 @@ export const team = [
     bio: 'Brian has had a longtime passion for helping people understand hugely complex software systems, starting with a Stanford Ph.D. and continuing through 10 years at Mozilla, where he worked on JavaScript VM optimizations and developed a precursor to Replay. He is a nomadic adventurer and enjoys sailing around Polynesia and van travel in the western US with his wife.'
   },
   {
-    image: dan,
+    image: vaughn,
     position: 'Software engineer',
-    name: 'Dan Miller',
-    bio: `Dan started his career at Etsy where he worked on PHP runtimes and type systems and has been trying to help developers be more productive ever since. When not trying to express the "is a hot dog a sandwich" question using types, he can probably be found hiking or skiing if the weather is nice, otherwise he's probably playing Super Smash Bros.`,
+    name: 'Brian Vaughn',
+    bio: `Brian enjoys building tools that help make other engineers more productive. Before joining Replay, he spent the past several years building and maintaining the React JS developer tools. When he's not writing software, Brian also enjoys making music and running.`,
     socials: {
-      twitter: 'https://twitter.com/jazzdan',
-      linkedin: 'https://www.linkedin.com/in/dan-miller-b3790211/'
+      twitter: 'https://twitter.com/brian_d_vaughn',
+      linkedin: 'https://www.linkedin.com/in/briandavidvaughn/'
     }
   },
+
   {
     image: logan,
     position: 'Software engineer',
@@ -49,10 +52,24 @@ export const team = [
     }
   },
   {
-    image: holger,
+    image: dan,
     position: 'Software engineer',
-    name: 'Holger Benl',
-    bio: `Holger explored the world of programming languages from 6502 machine code to extracting LISP programs from mathematical proofs. These days he's mostly interested in creating developer tools. When he's not working on Replay or one of his VSCode extensions, he likes to read political blogs and books and watch asian movies.`
+    name: 'Dan Miller',
+    bio: `Dan started his career at Etsy where he worked on PHP runtimes and type systems and has been trying to help developers be more productive ever since. When not trying to express the "is a hot dog a sandwich" question using types, he can probably be found hiking or skiing if the weather is nice, otherwise he's probably playing Super Smash Bros.`,
+    socials: {
+      twitter: 'https://twitter.com/jazzdan',
+      linkedin: 'https://www.linkedin.com/in/dan-miller-b3790211/'
+    }
+  },
+  {
+    image: mark,
+    position: 'Software engineer',
+    name: 'Mark Erikson',
+    bio: `Mark lives in southwest Ohio, USA and is a Redux maintainer, creator of Redux Toolkit, and general keeper of the Redux docs. He spends much of his time answering questions about React and Redux anywhere there's a comment box on the internet, and usually hangs out in the Reactiflux chat channels. Mark is also disturbed at the number of third-person references he has written in this bio.`,
+    socials: {
+      twitter: 'https://twitter.com/acemarke',
+      linkedin: 'https://www.linkedin.com/in/markerikson/'
+    }
   },
   {
     image: kannan,
@@ -78,7 +95,6 @@ export const team = [
     name: 'Jon Bell',
     bio: `Jon has been excited about designing things on the internet since the 80s, and has led design teams at Twitter and Windows Phone. He lives in windy Wellington, New Zealand with his large family and loves writing, teaching classes, playing soccer, and trying to remember how to play the clarinet.`,
     socials: {
-      twitter: 'https://twitter.com/JonBell',
       linkedin: 'https://www.linkedin.com/in/jon-bell-733310138/'
     }
   },
@@ -91,25 +107,12 @@ export const team = [
       linkedin: 'https://www.linkedin.com/in/theryanjduffy/'
     }
   },
+
   {
-    image: mark,
+    image: holger,
     position: 'Software engineer',
-    name: 'Mark Erikson',
-    bio: `Mark lives in southwest Ohio, USA and is a Redux maintainer, creator of Redux Toolkit, and general keeper of the Redux docs. He spends much of his time answering questions about React and Redux anywhere there's a comment box on the internet, and usually hangs out in the Reactiflux chat channels. Mark is also disturbed at the number of third-person references he has written in this bio.`,
-    socials: {
-      twitter: 'https://twitter.com/acemarke',
-      linkedin: 'https://www.linkedin.com/in/markerikson/'
-    }
-  },
-  {
-    image: vaughn,
-    position: 'Software engineer',
-    name: 'Brian Vaughn',
-    bio: `Brian enjoys building tools that help make other engineers more productive. Before joining Replay, he spent the past several years building and maintaining the React JS developer tools. When he's not writing software, Brian also enjoys making music and running.`,
-    socials: {
-      twitter: 'https://twitter.com/brian_d_vaughn',
-      linkedin: 'https://www.linkedin.com/in/briandavidvaughn/'
-    }
+    name: 'Holger Benl',
+    bio: `Holger explored the world of programming languages from 6502 machine code to extracting LISP programs from mathematical proofs. These days he's mostly interested in creating developer tools. When he's not working on Replay or one of his VSCode extensions, he likes to read political blogs and books and watch asian movies.`
   },
   {
     image: oliver,
@@ -120,5 +123,17 @@ export const team = [
       twitter: 'https://twitter.com/olliedealb',
       linkedin: 'https://www.linkedin.com/in/oliver-de-albuquerque-4a666934/'
     }
+  },
+  {
+    image: miriam,
+    position: 'Software engineer',
+    name: 'Miriam Budayr',
+    bio: `Miriam loves the thrill of solving difficult problems. She previously has worked as an open-source contributor on Firefox’s developer tools and also helped build video conferencing software for a telehealth platform. When she isn’t working she is playing Bach on the piano, swimming, playing Tetris, and spending time with family.`
+  },
+  {
+    image: domi,
+    position: 'Software engineer',
+    name: 'Dominik Seifert',
+    bio: `Dominik's two decades of software engineering adventures involved building MMORPG servers, optimizing CUDA algorithms and building/debugging full-stack applications. After getting frustrated about the inability to directly analyze his own systems' control and data flow, he dedicated his PhD dissertation to exploring and finding novel solutions for exactly that problem. Originally from Germany, he has been living in Taiwan for most of his adult life.`
   }
 ]
