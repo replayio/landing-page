07f734046e3b1f03d0e6a0123f9b25238b0954e6
JOYBOY-0 <falamino00@gmail.com>
2023-05-31 16:39:03 -0300

    implement section initial layout and logic

--- src/pages/time-travel/index.tsx
+++ src/pages/time-travel/index.tsx
@@ -1,5 +1,6 @@
 import { PageLayout } from '~/components/layout/page'
 import Prefooter from '~/components/sections/homepage/prefooter'
+import { DebugSpeed } from '~/components/sections/time-travel/debug-speed'
 import { Fundamentals } from '~/components/sections/time-travel/fundamentals'
 import { Hero } from '~/components/sections/time-travel/hero'
 import { TextReveal } from '~/components/sections/time-travel/text-reveal'
@@ -16,7 +17,7 @@ const TimeTravel = () => {
       >
         <TextReveal />
         <Fundamentals />
-        {/* debug  */}
+        <DebugSpeed />
         {/* roadmap */}
         <Prefooter />
       </div>
