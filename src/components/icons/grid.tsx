5923afba1e028f7efb2b4f3e7c98860b3fc793d5
JOYBOY-0 <falamino00@gmail.com>
2023-06-08 17:38:05 -0300

    Add icons

---
+++ src/components/icons/grid.tsx
@@ -0,0 +1,15 @@
+export const GridIcon = (props: JSX.IntrinsicElements['svg']) => (
+  <svg
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M1 8.25H11M1.5 6H10.5M2 3.75H10M6 1.5V10.5M4 1.75L3.25 10.25M8 1.75L8.75 10.25M0.5 10.5L2.0715 1.5H9.9285L11.5 10.5H0.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
