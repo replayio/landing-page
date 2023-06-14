593ee769f6b31070abd24bef90d823c2c97f4576
JOYBOY-0 <falamino00@gmail.com>
2023-06-13 21:13:29 -0300

    add svg and sky

--- src/components/sections/404/hero/index.tsx
+++ src/components/sections/404/hero/index.tsx
@@ -1,3 +1,4 @@
+import dynamic, { LoaderComponent } from 'next/dynamic'
 import Image from 'next/image'
 import { FC } from 'react'
 
@@ -7,14 +8,27 @@ import { ButtonLink } from '~/components/primitives/cta'
 import { SITEMAP } from '~/lib/sitemap'
 
 import s from './hero.module.scss'
+
+const Sky = dynamic(
+  () => import('~/components/common/sky').then((m) => m.Sky) as LoaderComponent,
+  {
+    ssr: false
+  }
+)
+
 export const Hero: FC = () => {
   return (
     <Section className={s.section}>
+      <div className={s['bg-container']}>
+        <div className={s['child']}>
+          <Sky />
+        </div>
+      </div>
       <Container className={s.container}>
         <h1 className="screen-reader-only">Page Not Found</h1>
         <div className={s.title}>
           <Image
-            src="/images/404/title.png"
+            src="/images/404/title.svg"
             fill
             alt="Page Not Found"
             draggable={false}
@@ -28,7 +42,7 @@ export const Hero: FC = () => {
           href={SITEMAP.home.href || '/'}
           aria-label={SITEMAP.home.label}
         >
-          Go {SITEMAP.home.label}
+          Go back {SITEMAP.home.label}
         </ButtonLink>
       </Container>
     </Section>
