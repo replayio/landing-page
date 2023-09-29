318bece1e05501855b1036da6cdab3711747f8ae
Brian Hackett <bhackett1024@gmail.com>
2023-09-29 14:00:42 -0700

    copy

--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -1,14 +1,45 @@
 import { Meta } from '~/components/common/meta'
 import { PageLayout } from '~/components/layout/page'
-import { Policy } from '~/components/sections/legal/policy'
+import { Section } from '~/components/layout/section'
+import { TitleAndSubtitle } from '~/components/primitives/texts'
 
-const Privacy = () => {
+const Overview = () => {
   return (
     <PageLayout>
-      <Meta title="Privacy Policy | Replay" />
-      <Policy />
+      <Meta title="Overview | Replay" />
+      <Section
+        id="homepage-overview"
+        className={s.section}
+        data-sitemap
+        data-sitemap-icon="workflow"
+        data-sitemap-short-title="Record"
+        data-sitemap-complete-title="Freeze bugs in time."
+        ref={inViewRef}
+      >
+      <TitleAndSubtitle
+        className={s.titleAndSubtitle}
+        title={{ children: 'Travel Back in Time', as: 'h2' }}
+        subtitle={{
+          children: (
+            <span>
+              Replay is a next generation time travel debugger like nothing you’ve seen before.  With low overhead the chrome based browser records just enough so it can be replayed exactly and inspected down to the finest detail.
+              <br></br>
+              <br></br>
+              <Link href="https://app.replay.io/team/new" aria-label="Create a team">
+                Learn More
+              </Link>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+            </span>
+          )
+        }}
+      />
     </PageLayout>
   )
 }
 
-export default Privacy
+export default Overview
