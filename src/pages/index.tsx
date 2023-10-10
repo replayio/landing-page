3354475134ef571df604ec3fe699235b8ec10a14
Jaril <jarilvalenciano@gmail.com>
2023-10-10 11:41:00 -0700

    Install intercom (#165)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { IntercomProvider } from 'react-use-intercom'
+
 import { PageLayout } from '~/components/layout/page'
 import { BugsSlider } from '~/components/sections/homepage/bugs-slider'
 import { DebugWithFriends } from '~/components/sections/homepage/debug-with-friends'
@@ -11,28 +13,32 @@ import { Security } from '~/components/sections/homepage/security'
 import { Testimonials } from '~/components/sections/homepage/testimonials'
 import { TrustedTeams } from '~/components/sections/homepage/trusted-teams'
 
+const INTERCOM_APP_ID = 'k7f741xx'
+
 const HomePage = () => {
   return (
-    <PageLayout>
-      <Hero />
-      <OrganizationTestimonials />
-      <div
-        style={{
-          background:
-            'linear-gradient(180deg, #000000 0%, rgba(13, 18, 29, 0.4) 12.28%, #0D121D 38.48%, #000000 54.71%, #000000 62.85%, #0D121D 72.4%, #000000 98.2%)'
-        }}
-      >
-        <BugsSlider />
-        <DeveloperTools />
-        <Quotes />
-        <DebugWithFriends />
-        <Features />
-        <Security />
-        <TrustedTeams />
-        <Testimonials />
-        <Prefooter />
-      </div>
-    </PageLayout>
+    <IntercomProvider appId={INTERCOM_APP_ID}>
+      <PageLayout>
+        <Hero />
+        <OrganizationTestimonials />
+        <div
+          style={{
+            background:
+              'linear-gradient(180deg, #000000 0%, rgba(13, 18, 29, 0.4) 12.28%, #0D121D 38.48%, #000000 54.71%, #000000 62.85%, #0D121D 72.4%, #000000 98.2%)'
+          }}
+        >
+          <BugsSlider />
+          <DeveloperTools />
+          <Quotes />
+          <DebugWithFriends />
+          <Features />
+          <Security />
+          <TrustedTeams />
+          <Testimonials />
+          <Prefooter />
+        </div>
+      </PageLayout>
+    </IntercomProvider>
   )
 }
 
