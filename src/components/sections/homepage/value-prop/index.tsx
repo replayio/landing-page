c94002fa2a11a5264ec35c98aa0c6cd1ee0eb327
Julian Benegas <julianbenegas99@gmail.com>
2023-11-06 20:52:47 -0300

    get hero and value prop data from basehub

--- src/components/sections/homepage/value-prop/index.tsx
+++ src/components/sections/homepage/value-prop/index.tsx
@@ -1,3 +1,4 @@
+import { RichText, RichTextProps } from 'basehub/react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,78 +6,41 @@ import { TitleAndSubtitle } from '~/components/primitives/texts'
 
 import s from './value-prop.module.scss'
 
-const ValueProp = () => {
-  const valuePropsData = [
-    {
-      title: 'Stop reproducing bugs',
-      description:
-        'Say goodbye to screenshots, videos, and repro steps. Recording a bug with Replay lets anyone debug it as if it happened on their own machine.',
-      link: '/overview#stop-reproducing-bugs',
-      linkText: 'Learn More'
-    },
-    {
-      title: 'Support your users with ease',
-      description:
-        'Bugs reported by QA and users with Replay are fully actionable and quickly understood by developers. Never close issues again for a lack of information.',
-      link: '/overview#support-your-users',
-      linkText: 'Learn More'
-    },
-    {
-      title: 'Fix all your flaky tests',
-      description:
-        'Debugging a flaky test failure with Replay is just as easy as any other bug. Stop struggling to understand these failures, or suppressing tests because they don’t pass reliably.',
-      link: '/overview#fix-all-your-flaky-tests',
-      linkText: 'Learn More'
-    },
-    {
-      title: 'Deploy with confidence',
-      description:
-        'Building a thorough and rock solid test suite is easy with Replay and really hard without it. This lets you deploy whenever you want and catch more issues before they hit users.',
-      link: '/overview#deploy-with-confidence',
-      linkText: 'Learn More'
-    },
-    {
-      title: 'Debug test failures remotely',
-      description:
-        'With Replay you can directly debug test failures that happened in CI as if they happened on your own machine. There is no need to locally reproduce the failure, or pore over logs or videos.',
-      link: '/overview#debug-test-failures-remotely',
-      linkText: 'Learn More'
-    },
-    {
-      title: 'Modernize your test suite',
-      description:
-        'Replay is easy to integrate with any test suite regardless of its framework. Get the best debugging experience in the world without rewriting any of your tests.',
-      link: '/overview#modernize-your-test-suite',
-      linkText: 'Learn More'
-    }
-  ]
+type ValuePropItem = {
+  key: string
+  title: string
+  content: unknown
+}
 
+const ValueProp = ({
+  title,
+  subtitle,
+  items
+}: {
+  title: string
+  subtitle: unknown
+  items: Array<ValuePropItem>
+}) => {
   return (
     <div className={s.componentWrapper}>
       <div className={s.timeTravelHero}>
         <TitleAndSubtitle
           title={{
             as: 'h2',
-            children: 'Travel Back in Time',
+            children: title,
             className: s.timeTravelHero
           }}
           subtitle={{
             children: (
-              <span className={s.timeTravelText}>
-                Replay is a next-generation time travel debugger. With low
-                overhead, the browser records just enough so it can be replayed
-                exactly and inspected down to the finest detail. <br />
-                <br />
-                <Link href="/overview">Learn more</Link>
-              </span>
+              <ValuePropSubtitleRenderer>{subtitle}</ValuePropSubtitleRenderer>
             )
           }}
         />
       </div>
 
       <div className={s.columns}>
-        {valuePropsData.map((item, index) => (
-          <React.Fragment key={index}>
+        {items.map((item, index) => (
+          <React.Fragment key={item.key}>
             <div className={s.valueProp}>
               <TitleAndSubtitle
                 title={{
@@ -85,25 +49,48 @@ const ValueProp = () => {
                   className: s.header
                 }}
                 subtitle={{
-                  children: <span>{item.description}</span>
+                  children: (
+                    <ValuePropSubtitleRenderer>
+                      {item.content}
+                    </ValuePropSubtitleRenderer>
+                  )
                 }}
               />
-              <div
-                style={{
-                  textAlign: 'center',
-                  marginTop: '10px',
-                  color: '#9ca3af',
-                  textDecoration: 'underline'
-                }}
-              >
-                <Link href={item.link}>{item.linkText}</Link>
-              </div>
             </div>
             {index === 1 && <hr className={s.divider} />}
           </React.Fragment>
         ))}
       </div>
     </div>
+  )
+}
+
+const ValuePropSubtitleRenderer = ({
+  children
+}: {
+  children: RichTextProps['children']
+}) => {
+  return (
+    <RichText
+      components={{
+        p: (props) => (
+          <span className={s.timeTravelText}>{props.children}</span>
+        ),
+        a: (props) => (
+          <Link
+            {...props}
+            style={{
+              textAlign: 'center',
+              marginTop: '10px',
+              color: '#9ca3af',
+              textDecoration: 'underline'
+            }}
+          />
+        )
+      }}
+    >
+      {children}
+    </RichText>
   )
 }
 
