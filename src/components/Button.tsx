c094c9e57ea8bd0f98379928524e79596f66698d
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2024-04-12 12:21:29 -0300

    upd

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,21 +4,21 @@ import styles from '../styles/Landingpage.module.css'
 
 const baseStyles = {
   solid:
-    'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2',
+    'group transition-colors ease-ease-in-out duration-100 inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2',
   outline:
-    'group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none'
+    'group transition-colors ease-ease-in-out duration-100 inline-flex ring-1 bg-white/50 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none'
 }
 
 const variantStyles = {
   solid: {
-    default: `${styles.bgPrimaryAccent} text-white hover:text-slate-100 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600`,
+    default: `bg-accent hover:bg-[#c61351] text-white hover:text-slate-100 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600`,
     blue: 'bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600',
     white:
       'bg-white text-slate-900 hover:bg-gray-50 active:bg-gray-200 active:text-slate-600 focus-visible:outline-white',
     gray: 'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white'
   },
   outline: {
-    default: `${styles.ringPrimaryAccent} `,
+    default: `ring-accent text-accent transition-colors shadow-none`,
     blue: 'ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300',
     white:
       'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
@@ -69,22 +69,22 @@ export function BaseHubButton({ className, label, ...props }: BaseHubButtonProps
   props.variant ??= 'solid'
   props.color ??= 'default'
 
-  className = clsx(
+  const variantClassName = clsx(
     baseStyles[props.variant as Variant],
-    props.variant === 'outline'
-      ? variantStyles.outline[props.color as keyof (typeof variantStyles)['outline']]
-      : props.variant === 'solid'
-        ? variantStyles.solid[props.color]
-        : undefined,
+    {
+      [variantStyles.outline[props.color as keyof (typeof variantStyles)['outline']]]:
+        props.variant === 'outline',
+      [variantStyles.solid[props.color]]: props.variant === 'solid'
+    },
     className
   )
 
   return typeof props.href === 'undefined' ? (
-    <button className={className} {...props}>
+    <button className={variantClassName} {...props}>
       {label}{' '}
     </button>
   ) : (
-    <Link className={className} {...props}>
+    <Link className={variantClassName} {...props}>
       {label}{' '}
     </Link>
   )
