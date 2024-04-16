e4acdb6bc26e8e23b664d8a54800b8b5cebb27e6
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2024-04-16 17:45:51 -0300

    upd

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -1,5 +1,4 @@
-import test from '@playwright/test'
-import expect from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/')
