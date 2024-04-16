19ab9bff4598efa390bb2ea6c18b8ce3bfb4642f
Jon Bell <github@lot23.com>
2024-04-16 14:36:13 +1200

    Adding Mixpanel to LP7

--- src/lib/analytics.tsx
+++ src/lib/analytics.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import { useEffect } from 'react'
-import { gaTrackingId } from './constants'
 import LogRocket from 'logrocket'
+import mixpanel from 'mixpanel-browser'
 
 export default function Analytics() {
   useEffect(() => {
-    LogRocket.init('4sdo4i/replay-landing-page')
+    LogRocket.init('4sdo4i/replay-docs')
+    mixpanel.init('ffaeda9ef8fb976a520ca3a65bba5014')
+    mixpanel.track('Loaded www.replay.io')
   }, [])
 
   return (
-    <script
-      async
-      src="https://analytics.umami.is/script.js"
-      data-website-id="ded9f3fb-cc9d-4c80-844a-742787b8b9db"
-    ></script>
+    <>
+      <script
+        async
+        src="https://analytics.umami.is/script.js"
+        data-website-id="9dab9357-6fa2-48ab-966a-82c4e1bb67fe"
+      ></script>
+      <script async src="https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"></script>
+    </>
   )
 }
