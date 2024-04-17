d2b7bfeec425f3db3528511408b3912ce8820164
Jon Bell <github@lot23.com>
2024-04-17 12:19:28 +1200

    Adding pageviews

--- src/lib/analytics.tsx
+++ src/lib/analytics.tsx
@@ -7,7 +7,9 @@ import mixpanel from 'mixpanel-browser'
 export default function Analytics() {
   useEffect(() => {
     LogRocket.init('4sdo4i/replay-docs')
-    mixpanel.init('ffaeda9ef8fb976a520ca3a65bba5014')
+    mixpanel.init('ffaeda9ef8fb976a520ca3a65bba5014', {
+      track_pageview: 'url-with-path'
+    })
     mixpanel.track('Loaded www.replay.io')
   }, [])
 
