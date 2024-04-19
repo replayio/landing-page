9ed858739c87141c13a5be7a5ab474504078cb1b
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2024-04-19 14:13:50 -0300

    upd

--- src/components/sections/Content.tsx
+++ src/components/sections/Content.tsx
@@ -134,7 +134,7 @@ export function Content({ content }: LandingPageFragment) {
             <Disclosure as="div" className="flex flex-col bg-white px-5 pb-[15px]">
               {({ open }) => (
                 <>
-                  <Disclosure.Button className="mt-3 flex items-center text-lg lg:text-2xl">
+                  <Disclosure.Button className="mt-3 flex items-center text-lg">
                     Chapter Index
                     <ChevronUpIcon
                       className={clsx(
