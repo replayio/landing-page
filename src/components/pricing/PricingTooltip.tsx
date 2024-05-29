ff50da3a93f432368b2f608b91196ea5b9dcb641
Jason Laster <jason.laster.11@gmail.com>
2024-05-29 16:39:49 -0700

    Update pricing table (#220)

--- src/components/pricing/PricingTooltip.tsx
+++ src/components/pricing/PricingTooltip.tsx
@@ -7,20 +7,26 @@ import Link from 'next/link'
 export const PricingTooltip = ({
   children,
   body,
-  learnMore
+  learnMore,
+  highlighted
 }: {
   learnMore?: string
   body: string
   children: React.ReactNode
+  highlighted?: boolean
 }) => {
   if (!body) return children
+  const tooltipClassName = highlighted
+    ? 'bg-indigo-500 border-indigo-200'
+    : 'border-slate-500 bg-slate-800'
+  const tooltipArrowClassName = highlighted ? 'fill-indigo-800' : 'fill-slate-800'
   return (
     <Tooltip.Provider>
       <Tooltip.Root>
         <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
         <Tooltip.Portal>
           <Tooltip.Content
-            className={`borderPrimaryAccent max-w-64 select-none rounded-[4px] border-t-4 border-red-500 bg-slate-100 px-[15px] py-[10px] text-[15px] text-sm leading-relaxed text-gray-800 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade`}
+            className={`${tooltipClassName} borderPrimaryAccent max-w-64 select-none rounded-[4px] border-t-4   px-[15px] py-[10px] text-[15px] text-sm leading-relaxed text-slate-100 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade`}
             sideOffset={0}
             alignOffset={0}
             align="start"
@@ -29,14 +35,14 @@ export const PricingTooltip = ({
               <p>{body}</p>
               {learnMore && (
                 <div className="mt-2">
-                  <Link className="text-gray-600 underline" href={learnMore}>
+                  <Link className="text-slate-200 underline" href={learnMore}>
                     Learn more
                   </Link>
                 </div>
               )}
             </div>
 
-            <Tooltip.Arrow className="fill-white" />
+            <Tooltip.Arrow className={tooltipArrowClassName} />
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
