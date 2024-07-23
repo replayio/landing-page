700ed6c90a80af93a18b149b899f2993357e90de
Dan Miller <dan@replay.io>
2024-07-23 14:36:52 -0700

    remove team members from about page

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -61,37 +61,10 @@ export const team = [
   },
 
   {
-    image: jon,
-    role: 'Designer',
-    name: 'Jon Bell',
-    bio: `Jon has been excited about designing things on the internet since the 80s, and has led design teams at Twitter and Windows Phone. He lives in windy Wellington, New Zealand with his large family and loves writing, teaching classes, playing soccer, and trying to remember how to play the clarinet.`,
-    socials: {
-      linkedin: 'https://www.linkedin.com/in/jon-bell-733310138/'
-    }
-  },
-  {
-    image: holger,
-    role: 'Software engineer',
-    name: 'Holger Benl',
-    bio: `Holger explored the world of programming languages from 6502 machine code to extracting LISP programs from mathematical proofs. These days he's mostly interested in creating developer tools. When he's not working on Replay or one of his VSCode extensions, he likes to read political blogs and books and watch asian movies.`
-  },
-  {
-    image: miriam,
-    role: 'Software engineer',
-    name: 'Miriam Budayr',
-    bio: `Miriam loves the thrill of solving difficult problems. She previously has worked as an open-source contributor on Firefox’s developer tools and also helped build video conferencing software for a telehealth platform. When she isn’t working she is playing Bach on the piano, swimming, playing Tetris, and spending time with family.`
-  },
-  {
     image: domi,
     role: 'Software engineer',
     name: 'Dominik Seifert',
     bio: `Dominik's two decades of software engineering adventures involved building MMORPG servers, optimizing CUDA algorithms and building/debugging full-stack applications. After getting frustrated about the inability to directly analyze his own systems' control and data flow, he dedicated his PhD dissertation to exploring and finding novel solutions for exactly that problem. Originally from Germany, he has been living in Taiwan for most of his adult life.`
-  },
-  {
-    image: colin,
-    role: 'Software engineer',
-    name: 'Colin Weld',
-    bio: `Colin helps build and scale Replay's backend. He hails from Oakland, California, and has been obsessed with data structures, invariants, and consistency since he first learned how to program. Prior to Replay, he worked on Backblaze's storage cloud. Outside of work, you can find him playing in a band, cooking, hiking, and rarely sitting still.`
   }
 ]
 
