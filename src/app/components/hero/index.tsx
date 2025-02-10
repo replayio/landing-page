c86cfcbe412fea22d3958be886596e0fe948f789
Brian Hackett <bhackett1024@gmail.com>
2025-02-10 09:48:59 -0800

    footers

--- src/app/components/hero/index.tsx
+++ src/app/components/hero/index.tsx
@@ -12,6 +12,9 @@ import { RichText } from 'basehub/react-rich-text'
 const HeroTitle = "An all seeing debugger for your web app";
 const HeroText = "We're building Nut, a chat API for explaining problems in your app.  Nut uses our lightweight recorder to capture and later query the billions of things that happened while the app was running.  Nut analyzes this data to tell you what is causing the bug.  Take the actual work out of figuring out most bugs.";
 
+const HeroFooter1 = "Nut works especially well with AI code writers.  AIs are lousy at debugging problems in code they’ve written.  The basic issue is a lack of context: people use devtools to understand their app, but AIs struggle here.  Nut gives AIs the clear explanations they need to fix bugs reliably.";
+const HeroFooter2 = "For now we're focusing on Nut.new, a no-code tool in the mold of Bolt, v0, and Lovable.  You can build complete apps from scratch by prompting the AI, and call the Nut API to get past the points where other tools get stuck.  Nut.new is free to use lightly, and in unlimited amounts through our early adopter program or with your own Anthropic API key.";
+
 export function Hero({ hero }: LandingPageFragment) {
   const { h1, h2 } = hero.heroVariants.items[0]
 
@@ -37,8 +40,16 @@ export function Hero({ hero }: LandingPageFragment) {
           <img
             src="/images/hero-image.jpg"
             alt="Nut Explanation"
-            className="w-[80%] md:w-1/2 h-auto mt-8 mx-auto"
+            className="w-[80%] md:w-[60%] h-auto mt-8 mx-auto"
           />
+          <div className="flex flex-col items-center">
+            <div className="flex-1 max-w-3xl mx-auto mt-6 rounded-lg bg-white/20 text-lg tracking-tight text-slate-700 text-center">
+              {HeroFooter1}
+            </div>
+            <div className="flex-1 max-w-3xl mx-auto mt-6 rounded-lg bg-white/20 text-lg tracking-tight text-slate-700 text-center">
+              {HeroFooter2}
+            </div>
+          </div>
         </Container>
       </div>
     </section>
