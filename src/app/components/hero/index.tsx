ba4fa0aa308fe7e89b6f83dd3202dde79608626e
Brian Hackett <bhackett1024@gmail.com>
2025-02-10 10:16:32 -0800

    wip

--- src/app/components/hero/index.tsx
+++ src/app/components/hero/index.tsx
@@ -96,8 +96,44 @@ export function Hero({ hero }: LandingPageFragment) {
         <Container className="relative z-10 flex w-full max-w-7xl flex-col pt-[60px] lg:pt-[90px] 2xl:pt-[120px]">
           <div className="flex flex-col items-center">
             <div className="flex-1 max-w-3xl mx-auto">
-              <h2 className="font-display text-3xl font-bold tracking-tight text-slate-900 sm:text-6xl xl:text-8xl text-center">
+              <h2 className="font-display text-2xl font-bold tracking-tight text-slate-900 sm:text-6xl xl:text-8xl text-center">
                 How it works
+              </h2>
+              <div className="flex-1 max-w-3xl mx-auto mt-6 rounded-lg bg-white/20 text-lg tracking-tight text-slate-700 text-center">
+                TODO
+              </div>
+            </div>
+          </div>
+        </Container>
+        <Container className="relative z-10 flex w-full max-w-7xl flex-col pt-[60px] lg:pt-[90px] 2xl:pt-[120px]">
+          <div className="flex flex-col items-center">
+            <div className="flex-1 max-w-3xl mx-auto">
+              <h2 className="font-display text-2xl font-bold tracking-tight text-slate-900 sm:text-6xl xl:text-8xl text-center">
+                Early adopters
+              </h2>
+              <div className="flex-1 max-w-3xl mx-auto mt-6 rounded-lg bg-white/20 text-lg tracking-tight text-slate-700 text-center">
+                TODO
+              </div>
+            </div>
+          </div>
+        </Container>
+        <Container className="relative z-10 flex w-full max-w-7xl flex-col pt-[60px] lg:pt-[90px] 2xl:pt-[120px]">
+          <div className="flex flex-col items-center">
+            <div className="flex-1 max-w-3xl mx-auto">
+              <h2 className="font-display text-2xl font-bold tracking-tight text-slate-900 sm:text-6xl xl:text-8xl text-center">
+                Future plans
+              </h2>
+              <div className="flex-1 max-w-3xl mx-auto mt-6 rounded-lg bg-white/20 text-lg tracking-tight text-slate-700 text-center">
+                TODO
+              </div>
+            </div>
+          </div>
+        </Container>
+        <Container className="relative z-10 flex w-full max-w-7xl flex-col pt-[60px] lg:pt-[90px] 2xl:pt-[120px]">
+          <div className="flex flex-col items-center">
+            <div className="flex-1 max-w-3xl mx-auto">
+              <h2 className="font-display text-2xl font-bold tracking-tight text-slate-900 sm:text-6xl xl:text-8xl text-center">
+                We're hiring!
               </h2>
               <div className="flex-1 max-w-3xl mx-auto mt-6 rounded-lg bg-white/20 text-lg tracking-tight text-slate-700 text-center">
                 TODO
