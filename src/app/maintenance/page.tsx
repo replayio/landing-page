e8835e19bdb2d508f3cd0fa39506e7106c391dd6
Brian Hackett <bhackett1024@gmail.com>
2026-01-29 13:21:35 -0800

    Add signup page for maintenance contracts

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,32 @@
+import { Suspense } from 'react'
+import { Metadata, Viewport } from 'next'
+import { Footer } from '~/components/Footer'
+import { Header } from '~/components/layout/header'
+
+import { TermsOfService } from '~/components/sections/legal/terms'
+
+export const metadata: Metadata = {
+  title: 'Terms of Service'
+}
+
+export const viewport: Viewport = {
+  themeColor: '#FFF'
+}
+
+const Terms = () => {
+  return (
+    <>
+      <Suspense fallback={null}>
+      <Header />
+      </Suspense>
+      <main>
+        <div className="px-4">
+          <TermsOfService />
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Terms
