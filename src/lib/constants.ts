86c390c3e0db31bf4775f125b97e69602fa3f445
Strider Wilson <strider.wilson@replay.io>
2026-03-10 15:12:44 -0400

    Try elongated version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -42,7 +42,7 @@ export const basementLog = `
 export const defaultMeta = {
   title: 'Replay - The MCP time travel debugger for your coding agent.',
   description: `Replay MCP gives your coding agent the runtime context it needs to fix its bugs.`,
-  ogImage: `${siteOrigin}/mstile-310x310.png`,
+  ogImage: `${siteOrigin}/mstile-310x150.png`,
   twitter: {
     handle: '@replayio',
     site: '@replayio'
