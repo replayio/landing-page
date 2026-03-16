99104f8a7f1a517ff14d00691dbf98e3f05c22f6
Strider Wilson <strider.wilson@replay.io>
2026-03-16 14:12:18 -0600

    Add linked in script to marketing pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,23 +46,52 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-function initApollo(){
-  var n = Math.random().toString(36).substring(7);
-  var o = document.createElement("script");
-  o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
-  o.async = true;
-  o.defer = true;
-  o.onload = function(){
-    window.trackingFunctions.onLoad({appId: "69987d99eda3b200117689e4"});
-  };
-  document.head.appendChild(o);
-}
-initApollo();
+              function initApollo(){
+                var n = Math.random().toString(36).substring(7);
+                var o = document.createElement("script");
+                o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+                o.async = true;
+                o.defer = true;
+                o.onload = function(){
+                  window.trackingFunctions.onLoad({appId: "69987d99eda3b200117689e4"});
+                };
+                document.head.appendChild(o);
+              }
+              initApollo();
+            `.trim()
+          }}
+        />
+        <Script
+          id="linkedin-insight-tag"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window._linkedin_partner_id = "8830930";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+              (function(l) {
+                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
             `.trim()
           }}
         />
       </head>
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8830930&fmt=gif"
+          />
+        </noscript>
         <TopBanner />
         {children}
       </body>
