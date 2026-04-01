6e0e10abf94a7de9ad960e1e1b6f8848d6db8e89
Strider <strider.wilson@replay.io>
2026-04-01 10:05:01 -0400

    Remove announcement button (#281)

--- src/app/precog/constants.ts
+++ src/app/precog/constants.ts
@@ -1,4 +1,1 @@
 export const PRECOG_DOCS_MCP_QUICKSTART = 'https://docs.replay.io/basics/replay-mcp/quickstart'
-
-export const PRECOG_BLOG_ANNOUNCEMENT =
-  'https://replay-marketing-site-v2.netlify.app/blog/introducing-replay-precog'
